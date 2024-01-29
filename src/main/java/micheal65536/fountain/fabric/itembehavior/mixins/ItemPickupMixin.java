fa3c34bd137020223dcc92ae8b5154092f4f926d
micheal65536 <>
2024-01-29 01:31:27 +0000

    Allow Earth players to pick up item entities by tapping on them

---
+++ src/main/java/micheal65536/fountain/fabric/itembehavior/mixins/ItemPickupMixin.java
@@ -0,0 +1,27 @@
+package micheal65536.fountain.fabric.itembehavior.mixins;
+
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+
+import micheal65536.fountain.fabric.earthmode.EarthModePlayer;
+
+// allows Earth players to pick up item entities by tapping on them
+@Mixin(ItemEntity.class)
+public class ItemPickupMixin
+{
+	public ActionResult interact(PlayerEntity player, Hand hand)
+	{
+		if (((EarthModePlayer) player).isEarthMode())
+		{
+			((ItemEntity) (Object) this).onPlayerCollision(player);
+			return ActionResult.CONSUME;
+		}
+		else
+		{
+			return ActionResult.PASS;
+		}
+	}
+}
