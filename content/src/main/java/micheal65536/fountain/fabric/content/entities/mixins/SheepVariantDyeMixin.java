bef82f8e5165e8551bed2a91e1782ff5a61a13ca
micheal65536 <>
2024-04-23 10:55:24 +0100

    Fixed syntax error in mixin

--- content/src/main/java/micheal65536/fountain/fabric/content/entities/mixins/SheepVariantDyeMixin.java
+++ content/src/main/java/micheal65536/fountain/fabric/content/entities/mixins/SheepVariantDyeMixin.java
@@ -16,7 +16,7 @@ import micheal65536.fountain.fabric.content.entities.SheepVariantEntity;
 @Mixin(DyeItem.class)
 public class SheepVariantDyeMixin
 {
-	@Inject(method = "useOnEntity(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult", at = @At(value = "HEAD"), cancellable = true)
+	@Inject(method = "useOnEntity(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;", at = @At(value = "HEAD"), cancellable = true)
 	private void useOnEntity(ItemStack itemStack, PlayerEntity playerEntity, LivingEntity livingEntity, Hand hand, CallbackInfoReturnable<ActionResult> callbackInfo)
 	{
 		if (livingEntity instanceof SheepVariantEntity && !((SheepVariantEntity) livingEntity).dyeable)
